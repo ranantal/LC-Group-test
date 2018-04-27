65fa02da8619c3cc38acac2c6e8701a177aab1f1
Nikita Bychkov <dichlofox@gmail.com>
2018-04-27 23:22:23 +0700

    deleted useless functions from data service

--- src/app/_services/data.service.ts
+++ src/app/_services/data.service.ts
@@ -8,28 +8,9 @@ export class DataService {
   private filteredUsersSource = new BehaviorSubject<User[]>(null);
   filteredUsers = this.filteredUsersSource.asObservable();;
 
-  private genderFilterSource = new BehaviorSubject<string>("default message");
-  genderFilter = this.genderFilterSource.asObservable();
-  private departmentFilterSource = new BehaviorSubject<string>("default message");
-  departmentFilter = this.departmentFilterSource.asObservable();
-  private cityFilterSource = new BehaviorSubject<string>("default message");
-  cityFilter = this.cityFilterSource.asObservable();
-
   constructor() { }
 
   changeFilteredUsers(filteredUsers: User[]): void {
     this.filteredUsersSource.next(filteredUsers);
-  }
-
-  changeGenderFilter(genderFilter: string) :void {
-    this.genderFilterSource.next(genderFilter);
-  }
-
-  changeDepartmentFilter(departmentFilter: string): void {
-    this.departmentFilterSource.next(departmentFilter);
-  }
-
-  changeCityFilter(cityFilter: string): void {
-    this.cityFilterSource.next(cityFilter);
   }
 }
